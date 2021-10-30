734384709a6d2469b603fd18035750a4e4c118f7
falconbyte <89982358+falconbyte@users.noreply.github.com>
2021-10-30 14:33:42 +0200

    Update

--- src/blog/DatentypenKonvertieren.java
+++ src/blog/DatentypenKonvertieren.java
@@ -1,8 +1,29 @@
+// Visit https://falconbyte.net
 package blog;
 
 public class DatentypenKonvertieren {
 
     public static void main(String[] args){
+
+        //Statische MEthoden der Wrapper-Klassen
+
+        // String in primitiven Wert umwandeln
+        int i = Integer.parseInt("42");
+        double d = Double.parseDouble("42");
+
+        // String in korrespondierende Wrapper-Objekte umwalden
+        Integer iW = Integer.valueOf("42");
+        Double dW = Double.valueOf("42");
+
+        // 1. If the string is not an integer, NumberFormatException will be thrown.
+        // 2. NAtürlich geht das mit den anderen primitiven Typen auch (Byte, Long, Float)
+
+        // Primitiven Wert in String umwandeln
+        String strI = Integer.toString(42);
+        String strD = Double.toString(42.0);
+        // NAtürlich durch Autounboxing auch korrespondierende Wrapper-Objekte einsetzen:
+        String strIW = Integer.toString(iW);
+
 
     }
 
--- src/blog/DatentypenKonvertierenBeispiel.java
+++
@@ -1,9 +0,0 @@
-package blog;
-
-public class DatentypenKonvertierenBeispiel {
-
-    public static void main(String[] args){
-
-    }
-
-}
